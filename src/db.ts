52ca56b9c84839acc64a11e59a9a5a0033415844
Igor Kasevich <igorkasevich@gmail.com>
2019-10-19 19:30:49 +0300

    Extrat db helper functions

--- src/db.ts
+++ src/db.ts
@@ -3,13 +3,4 @@ import leveldown from "leveldown";
 
 const db = levelup(leveldown("./db"));
 
-export function putValue(key: string, value: unknown) {
-  return db.put(key, JSON.stringify(value));
-}
-
-export async function getValue<T>(key: string): Promise<T> {
-  const bytes = await db.get(key);
-  return JSON.parse(String(bytes));
-}
-
 export default db;
---
+++ src/utils/dbHelpers.ts
@@ -0,0 +1,18 @@
+import { LevelUp } from "levelup";
+import { LevelDown } from "leveldown";
+import { AbstractIterator } from "abstract-leveldown";
+
+export async function putValue(db: Db, key: string, value: unknown) {
+    return db.put(key, JSON.stringify(value));
+}
+
+export async function getValue<T>(db: Db, key: string): Promise<T> {
+  try {
+    const bytes = await db.get(key);
+    return JSON.parse(String(bytes));
+  } catch (e) {
+    return null;
+  }
+}
+
+export type Db = LevelUp<LevelDown, AbstractIterator<any, any>>;
