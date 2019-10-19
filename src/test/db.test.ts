65747f25fecd6e133b457f4d2a9e3581e775f6ac
Igor Kasevich <igorkasevich@gmail.com>
2019-10-19 19:29:35 +0300

    Rename tests folder

---
+++ src/test/db.test.ts
@@ -0,0 +1,26 @@
+import { putValue, getValue } from "../utils/dbHelpers";
+import db from "../db";
+
+describe("db functionality", () => {
+  test("putValue and getValue functions", async () => {
+      expect.assertions(1);
+      const key = JSON.stringify([
+        "test",
+        "destination1",
+        "destination2",
+        "bicycle"
+      ]);
+      const value = 42;
+      await putValue(db, key, value);
+      const valueFound = await getValue(db, key);
+      expect(valueFound).toBe(value);
+  });
+
+  test("getValue non existing key", async () => {
+    const key = JSON.stringify(["unknown"]);
+    const val = await getValue(db, key);
+  });
+  afterAll(() => {
+    db.close();
+  })
+});
