15589ffc7d86e6c91333f156804bbf45094b4967
Igor Kasevich <igorkasevich@gmail.com>
2019-10-19 14:38:34 +0300

    Format code

--- src/__tests__/Datastore.test.ts
+++ src/__tests__/Datastore.test.ts
@@ -1,4 +1,11 @@
-import { SPLITTER, LINE_SPLITTER, parseEntry, studentTypeMap, practiceTypeMap, parseEntries } from "../utils/Parser";
+import {
+  SPLITTER,
+  LINE_SPLITTER,
+  parseEntry,
+  studentTypeMap,
+  practiceTypeMap,
+  parseEntries
+} from "../utils/Parser";
 import { IStudent } from "../typings/IStudent";
 import { IPractice } from "../typings/IPractice";
 
@@ -6,7 +13,11 @@ describe("Entry parsing", () => {
   test("one student parsing", () => {
     const titleLine = `id	address	alternativeAddress1	alternativeAddress2	hasCar	hasChildren	favoriteSpecialties`;
     const studentLine = `S001	Am Waldacker 21c, 60388 Frankfurt am Main	Pforzheimer Straße 15, 60329 Frankfurt am Main	Im Wörth 8, 60433 Frankfurt am Main	0	1	Notfallmedizin, Kinder-Endokrinologie und -Diabetologie`;
-    const student = parseEntry(studentLine, titleLine.split("\t"), studentTypeMap);
+    const student = parseEntry(
+      studentLine,
+      titleLine.split("\t"),
+      studentTypeMap
+    );
     expect(student).toMatchObject<IStudent>({
       address: "Am Waldacker 21c, 60388 Frankfurt am Main",
       alternativeAddress1: "Pforzheimer Straße 15, 60329 Frankfurt am Main",
@@ -24,11 +35,15 @@ describe("Entry parsing", () => {
   test("one practice parsing", () => {
     const titleLine = `id	address	specialties`;
     const practiceLine = `P001	Rhaban-Fröhlich-Straße 11, 60433 Frankfurt am Main	Sportmedizin, Notfallmedizin`;
-    const student = parseEntry(practiceLine, titleLine.split("\t"), practiceTypeMap);
+    const student = parseEntry(
+      practiceLine,
+      titleLine.split("\t"),
+      practiceTypeMap
+    );
     expect(student).toMatchObject<IPractice>({
       address: "Rhaban-Fröhlich-Straße 11, 60433 Frankfurt am Main",
       id: "P001",
-      specialties: ["Sportmedizin", "Notfallmedizin"],
+      specialties: ["Sportmedizin", "Notfallmedizin"]
     });
   });
 
@@ -36,8 +51,16 @@ describe("Entry parsing", () => {
     const titleLine = `id	address	alternativeAddress1	alternativeAddress2	hasCar	hasChildren	favoriteSpecialties`;
     const studentLine = `S001	Am Waldacker 21c, 60388 Frankfurt am Main	Pforzheimer Straße 15, 60329 Frankfurt am Main	Im Wörth 8, 60433 Frankfurt am Main	0	0	Notfallmedizin, Kinder-Endokrinologie und -Diabetologie`;
     const studentLine2 = `S002	Storchgasse 2, 65929 Frankfurt am Main	Triebstraße 45, 60388 Frankfurt am Main	Eifelstraße 51, 60529 Frankfurt am Main	0	0	Notfallmedizin, Hämostaseologie`;
-    const correctStudent1 = parseEntry(studentLine, titleLine.split(SPLITTER), studentTypeMap);
-    const correctStudent2 = parseEntry(studentLine2, titleLine.split(SPLITTER), studentTypeMap);
+    const correctStudent1 = parseEntry(
+      studentLine,
+      titleLine.split(SPLITTER),
+      studentTypeMap
+    );
+    const correctStudent2 = parseEntry(
+      studentLine2,
+      titleLine.split(SPLITTER),
+      studentTypeMap
+    );
     const CSVData = [titleLine, studentLine, studentLine2].join(LINE_SPLITTER);
     const students = parseEntries(CSVData, studentTypeMap);
     expect(students).toHaveLength(2);
--- src/__tests__/db.test.ts
+++ src/__tests__/db.test.ts
@@ -3,10 +3,10 @@ import db, { putValue, getValue } from "../db";
 describe("db functionality", () => {
   test("db get/put functionality", async () => {
     expect.assertions(1);
-    const key = JSON.stringify(["test","destination1", "destination2", "car"]);
+    const key = JSON.stringify(["test", "destination1", "destination2", "car"]);
     const value = JSON.stringify(34765);
     await db.put(key, value);
-    const valueFound = await db.get(key).then((bytesValue) => {
+    const valueFound = await db.get(key).then(bytesValue => {
       return JSON.parse(String(bytesValue));
     });
     await db.del(key);
@@ -15,7 +15,12 @@ describe("db functionality", () => {
 
   test("putValue and getValue functions", async () => {
     expect.assertions(1);
-    const key = JSON.stringify(["test", "destination1", "destination2", "bicycle"]);
+    const key = JSON.stringify([
+      "test",
+      "destination1",
+      "destination2",
+      "bicycle"
+    ]);
     const value = 42;
     await putValue(key, value);
     const valueFound = await getValue(key);
@@ -25,5 +30,5 @@ describe("db functionality", () => {
 
   afterAll(async () => {
     await db.close();
-  })
+  });
 });
